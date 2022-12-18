e7f16b0a77dd02eb32ebda067f32dc1d6f89b25b
Brodin-Fad <94728060+Brodin-Fad@users.noreply.github.com>
2022-12-17 17:24:31 -0800

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,31 @@
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start Code
+console.log("Room Name -" + Room_names);
+row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_Names +"</div><hr>"
+//End code
+});});}
+getData();
+
+    
+function addRoom()
+{
+      room_name = document.getElementById("room_name").value;
+
+      firebase.database().ref("/").child(room_name).update({
+            purpose : "adding room name"
+      });
+
+      localStorage.setITem("room_name" , room_name);
+
+      window.location = "kwitter_page.html";
+}
+
+
+function redirectToRoomName()
+{
+    localStorage.getItem("room_name" , room_name);
+
+    window.location = "kwitter_page.html";
+}
